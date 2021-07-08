111f71c52550e781dea4daed0e05c23195e786c8
yotam-bp <yotyyybp@gmail.com>
2021-07-08 20:59:42 +0300

    media size

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -32,7 +32,7 @@ const Projects = () => {
 
             return (
               <Row key={id}>
-                <Col lg={4} sm={12}>
+                <Col lg={5} sm={12}>
                   <Fade
                     left={isDesktop}
                     bottom={isMobile}
@@ -71,7 +71,7 @@ const Projects = () => {
                     </div>
                   </Fade>
                 </Col>
-                <Col lg={8} sm={12}>
+                <Col lg={7} sm={12}>
                   <Fade
                     right={isDesktop}
                     bottom={isMobile}
